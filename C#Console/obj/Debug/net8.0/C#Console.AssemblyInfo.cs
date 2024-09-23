8bb37a5dd95f265e65388c819b4ca2f575007b6f
25mappslc <99293873+25mappslc@users.noreply.github.com>
2024-09-23 13:46:08 -0500

    Today's lesson: dictionaries

--- C#Console/obj/Debug/net8.0/C#Console.AssemblyInfo.cs
+++ C#Console/obj/Debug/net8.0/C#Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f62ad891d4a6215368ff91805443d9fbfb99ef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
